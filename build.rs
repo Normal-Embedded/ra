31e0a87956a73ac2700ebe4321278776c3228884
kobe uyeda <kobe@normallab.co>
2025-11-09 15:21:31 -1000

    save

---
+++ build.rs
@@ -0,0 +1,18 @@
+use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
+fn main() {
+    if env::var_os("CARGO_FEATURE_RT").is_some() {
+        let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+        if env::var_os("CARGO_FEATURE_RA4M1").is_some() {
+            File::create(out.join("device.x"))
+                .unwrap()
+                .write_all(include_bytes!("device-ra4m1.x"))
+                .unwrap();
+        }
+        println!("cargo:rustc-link-search={}", out.display());
+        println!("cargo:rerun-if-changed=device.x");
+    }
+    println!("cargo:rerun-if-changed=build.rs");
+}
